14e0dc59d6e8cf10aa735922334c74465f336177
SacoDePapas <sgomez15@alumnos.uaq.mx>
2025-03-28 12:20:38 -0600

    Inicio de lo orientado a objetos

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 from flask import Flask 
+from gemini import *
+from mongo import *
+from cloudflare import *
+
+
---
+++ mongo.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+import pymongo
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+uri = os.getenv("DB_URL")
+client = MongoClient(uri, server_api=pymongo.server_api.ServerApi(
+ version="1", strict=True, deprecation_errors=True))
+
+
