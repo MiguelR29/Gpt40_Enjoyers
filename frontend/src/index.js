184f63dce9e2c787b2a619140574f06cf87c0f76
frolpe <frolpe@outlook.com>
2025-03-29 01:03:34 -0600

    login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-cge65vpczx1g7bl2.us.auth0.com"
+      clientId="0AWaN73j3cXo1NUHXaoxAydsRDbGXmjK"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,36 +1,14 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "../styles/login.css";
-import imgPrincipal from "../img/a-chosen-soul-aZyP_xZUpVk-unsplash.jpg";
+import React, { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Login() {
-    const navigate = useNavigate();
+    const { loginWithRedirect } = useAuth0();
 
-    const handleClick = (event) => {
-        event.preventDefault();
-        navigate("/");
-    };
+    useEffect(() => {
+        loginWithRedirect();
+    }, [loginWithRedirect]);
 
-    return (
-        <div className="contenedorPrincipal">
-            <div className="capaBlur"></div>
-            <div className="contenedorImg">
-                <img src={imgPrincipal} alt="logDiscret" />
-            </div>
-            <div className="contenedorForm">
-                <h1 className="login-title">Iniciar sesión</h1>
-                <form className="login-form">
-                    <label htmlFor="username" className="login-label">Usuario</label>
-                    <input type="text" id="username" className="login-input" />
-
-                    <label htmlFor="password" className="login-label">Contraseña</label>
-                    <input type="password" id="password" className="login-input" />
-
-                    <button type="submit" className="login-button" onClick={handleClick}>Ingresar</button>
-                </form>
-            </div>
-        </div>
-    );
+    return null; // No necesitas renderizar nada ya que redirige automáticamente
 }
 
 export default Login;
