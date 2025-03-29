590901682890725ae9d3171f30e255bf78baea5a
frolpe <frolpe@outlook.com>
2025-03-29 07:33:40 -0600

    cambios x

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -5,7 +5,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const ProtectedRoute = ({ element }) => {
     const { isAuthenticated, isLoading } = useAuth0();
 
-    if (isLoading) return <Navigate to="/" />; 
+    if (isLoading) return <p>Cargando...</p>; // Evita redirección prematura
 
     return isAuthenticated ? element : <Navigate to="/" />;
 };
